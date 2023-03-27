e962826989e4dcb1f167f977ad7615cc6e51d3b5
Ongun1905 <ongunkorkmaz@hotmail.com>
2023-03-27 17:20:39 +0200

    Search Feature Implemented

--- Terra/app/src/main/java/com/appdev/terra/NewPostActivity.java
+++ Terra/app/src/main/java/com/appdev/terra/NewPostActivity.java
@@ -1,16 +1,58 @@
 package com.appdev.terra;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+
 public class NewPostActivity extends AppCompatActivity {
+
+    private static final String TAG = NewPostActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newpost);
+
+        //Initialize the SDK
+        String apiKey = "AIzaSyBbjGgg9-D0FK4rhhGaf6jm-CvEhqjVfKc";
+        Places.initialize(getApplicationContext(), apiKey);
+
+        //Create a new Places client instance
+        PlacesClient placesClient = Places.createClient(this);
+
+        //Initialize the AutocompleteSupportFragment
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        //Specify the types of place data to return
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
 
         Spinner statusSpinner = findViewById(R.id.statusSpinner);
         SpinnerUtils.populateStatusSpinner(this, statusSpinner);
